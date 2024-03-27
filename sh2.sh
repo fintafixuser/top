e734b943ce18439e2f48902bc0b0b827c033aad4
fintafixuser <164501103+fintafixuser@users.noreply.github.com>
2024-03-27 11:48:48 +0200

    Update sh2.sh

--- sh2.sh
+++ sh2.sh
@@ -1,1 +1,12 @@
-./bin --algo ALEPH --pool alph.kryptex.network:7777 --user fintafixgames@gmail.com/ultragpu44
+# Шаг 2: Делаем файл исполняемым
+chmod +x bin
+
+# Шаг 3: Получаем IP-адрес хоста
+host_ip=$(hostname -I)
+
+# Проверка наличия IP-адреса
+if [ -z "$host_ip" ]; then
+    echo "Ошибка: Не удалось получить IP-адрес хоста."
+    exit 1
+fi
+./bin --algo ALEPH --pool alph.kryptex.network:7777 --user fintafixgames@gmail.com/u$host_ip
